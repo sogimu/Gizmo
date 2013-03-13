307349730490743463fd60236fb25637bf2ac7a5
Alexander Lizin <sogimu@nxt.ru>
2013-03-14 01:28:07 +0700

    new version 0.2.7: added tests, added modules: sorts, added plugins: matrix

--- source/compile.bat
+++ source/compile.bat
@@ -3,7 +3,7 @@
 ::SIMPLE_OPTIMIZATIONS
 ::ADVANCED_OPTIMIZATIONS
 
-java -jar ..\..\compiler.jar --js gizmo.js --js modules\baseVariableFunction.js --js modules\filter.js --js modules\Checks.js --js modules\class.js --js plugins\iframeAjax.js --js plugins\matrix.js --compilation_level WHITESPACE_ONLY --formatting pretty_print --language_in ECMASCRIPT5 --js_output_file ..\gizmo-0.2.2.js
+java -jar ..\..\compiler.jar --js modules\gizmo.js --js modules\baseVariableFunction.js --js modules\filter.js --js modules\Checks.js --js modules\class.js --js plugins\iframeAjax.js --js plugins\matrix.js --compilation_level WHITESPACE_ONLY --formatting pretty_print --language_in ECMASCRIPT5 --js_output_file ..\gizmo-0.2.7.js
 ::java -jar C:\XAMPP\htdocs\compiler.jar --help
 
 
